6e499729c5b32d605015c1fea76c4a62f6ece208
kiutba <kiutba@pop-os.localdomain>
2023-06-23 15:58:42 +0100

    Updated index.html

--- frontend/main.js
+++ frontend/main.js
@@ -1,11 +1,12 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 })
-const functionApi = 'http://localhost:7071/GetResumeCounter';
+const functionAPIUrl = 'https://getresumecounterkelly.azurewebsites.net/GetResumeCounter?code=Fk81OM7TRAulctda272poAkCvKRuPr0Bs6ogLI2SjBEqAzFu-IzjnA==';
+const LocalFunctionApi = 'http://localhost:7071/GetResumeCounter';
 
 const getVisitCount = () => {
     let count =30;
-    fetch(functionApi).then(response => {
+    fetch(functionAPIUrl).then(response => {
         return response.json()
 }).then (response => {
     console.log("Website called funtion API.");
